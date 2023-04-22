51ee9f9b590d2e2c6d1eeb8afd6aa36ce7b864e3
PrajktaPatil5 <131451709+PrajktaPatil5@users.noreply.github.com>
2023-04-22 18:20:42 +0530

    added

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,11 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+public class Program{
+  
+  public static void Main(string[] args)
+  {
+    BankAccount b = new BankAccount("Prajkta", 20000);
+    Console.WriteLine($"Account  {b.Number} was  created for {b.Owner} with {b.Balance} ");
+  }
+}
