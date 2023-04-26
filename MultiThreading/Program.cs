2b418a112b58a74488d7e4d316875a181e7b65b3
PrajktaPatil5 <131451709+PrajktaPatil5@users.noreply.github.com>
2023-04-26 11:41:31 +0530

    first commit

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Sample
+{
+   public static void Main()
+   {
+       Example e = new Example();
+       Thread t = new Thread(new ThreadStart(e.FirstThread));
+       t.Start();
+       Console.Read();
+   }
+}
